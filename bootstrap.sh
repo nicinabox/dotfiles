db3248b47ec613178ae551cee637726d0ab05283
Nic Aitch <nic@nicinabox.com>
2015-07-25 12:31:10 -0500

    Update boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 # Fix /usr/local permissions
-chown `whoami` /usr/local
+sudo chown `whoami` /usr/local
 
-# Install brew
+# Brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install node
+# Taps
 brew tap homebrew/dupes
 brew tap homebrew/services
 
+# Libs and dependencies
 brew install caskroom/cask/brew-cask
 brew install gpg2
 brew install graphicsmagick
@@ -16,6 +17,7 @@ brew install nano
 brew install tmux
 brew install tree
 brew install z
+brew install zsh-syntax-highlighting
 brew install heroku-toolbelt
 brew install reattach-to-user-namespace
 brew install watchman
@@ -23,8 +25,18 @@ brew install postgresql
 brew install redis
 brew install node
 
-echo "Don't forget to finish installers for postgres and redis."
+# Services
+brew services start postgresql
+brew services start redis
 
+# rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\\curl -sSL https://get.rvm.io | bash -s stable
+
+# Applications
 brew cask google-chrome
 brew cask jumpcut
-
+brew cask spotify
+brew cask slack
+brew cask alfred
+brew cask the-unarchiver
