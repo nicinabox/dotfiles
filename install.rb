5c71ca1186f708672def0576d471ee79b7d90fbb
Nic Aitch <nic@nicinabox.com>
2015-12-12 00:44:14 -0600

    Update install.rb to include uninstalling/overwriting

--- install.rb
+++ install.rb
@@ -1,20 +1,40 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'optparse'
 
-$home = File.expand_path('~')
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby install.rb [options]'
+
+  opts.on('-f', 'Force install dotfiles') do |f|
+    options[:force] = f
+  end
+
+  opts.on('-d', 'Delete installed dotfiles') do |d|
+    options[:delete] = d
+  end
+end.parse!
+
+HOME_DIR = File.expand_path('~')
+
+def exists? dest
+  !(File.symlink? dest or File.exists? dest)
+end
 
 def symlink file, dest
-  unless File.symlink? dest or File.exists? dest
+  if exists? dest
     src = File.expand_path(file)
     puts "#{file} -> #{dest}"
     FileUtils.ln_s src, dest
+  else
+    puts "Skipping #{file}"
   end
 end
 
 def target file
   name = File.join(file.split('/').drop(1))
-  File.join($home, ".#{name}")
+  File.join(HOME_DIR, ".#{name}")
 end
 
 def mkdir file
@@ -24,12 +44,34 @@ def mkdir file
   FileUtils.mkdir_p path
 end
 
-Dir['src/**'].each do |file|
-  symlink file, target(file)
+def rm file
+  puts "Removing #{file}"
+  FileUtils.rm file
 end
 
-Dir['extras/**/**'].select {|f| File.file? f }.each do |file|
-  t = target file
-  mkdir t
-  symlink file, t
+def install_dotfiles
+  Dir['src/**'].each do |file|
+    symlink file, target(file)
+  end
+
+  Dir['extras/**/**'].select {|f| File.file? f }.each do |file|
+    t = target file
+    mkdir t
+    symlink file, t
+  end
+end
+
+def uninstall_dotfiles
+  Dir['src/**'].each do |file|
+    rm target(file)
+  end
+end
+
+if options[:force]
+  uninstall_dotfiles
+  install_dotfiles
+elsif options[:delete]
+  uninstall_dotfiles
+else
+  install_dotfiles
 end
